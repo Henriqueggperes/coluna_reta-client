221cf4815c4138ecde7ff2a889c3c0bc09c9ccdf
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-08-09 23:11:45 -0300

    feat(Backoficce): Backoficce Homepage Header Style

---
+++ app/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+import logo from './../../assets/icons/cr_logo.png'
+import user from './../../assets/icons/user.svg'
+import exit from './../../assets/icons/exit.svg'
+
+const Header = () => {
+  return (
+      <header className="backoficce-header">
+        <div className="header_logo-container">
+         <img src={logo} alt="Logo roxo do Coluna Reta Backoficce" className="header-logo" />
+         <span className="header_logo-span">Backoficce</span>
+        </div>
+        <div className="header_user_card-container">
+            <img src={user} alt="Imagem do usuário" className="header_user_card-image"  />
+            <img src={exit} alt="Icone para sair da plataforma" className="header_user_card_exit-icon" />
+        </div>
+      </header>
+  );
+};
+
+export default Header;
---
+++ app/src/pages/Backoficce/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+const Backoficce = () => {
+  return (
+      <main className='backoficce_main-container'>
+         <Header></Header>
+      </main>
+    )
+}
+
+export default Backoficce
