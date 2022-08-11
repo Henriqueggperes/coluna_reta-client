4e817048e5a3b83a464b7ce70e7ad1d40eca30fd
Keven Ferreira Moraes <55964670+Kevenfz@users.noreply.github.com>
2022-08-10 21:17:26 -0300

    style: Login Page estilizada, tudo ok!

--- app/src/pages/LoginPage/index.tsx
+++ app/src/pages/LoginPage/index.tsx
@@ -1,28 +1,48 @@
-import React from 'react'
-import  LoginImg from "../../assets/img/login_char.svg";
+import React from "react";
+import LoginImg from "../../assets/img/login_char.svg";
 import Logo from "../../assets/icons/cr_logo.png";
 import "./style.css";
 
 export const LoginPage = () => {
-    return (
-        <section className='MainSection'>
-            <section className="card-login">
-                <form>
-                    <label htmlFor="email">E-mal</label>
-                    <input type="email" name="email" id="email" />
-                    
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" />
+  return (
+    <section className="background">
+      <section className="MainSection">
+        <section className="left-side">
+          <div className="card-login">
+            <form>
+              <h1>Bem vindo!</h1>
+              
+              <div className="single-input">
+                <input required className="input" type="email" name="email" id="email" />
+                <label htmlFor="email">E-mail</label>
+              </div>
+              
+              <div className="single-input">
+                <input required className="input" type="password" name="password" id="password" />
+                <label htmlFor="password">Password</label>
+              </div>
 
-                    <button type="submit">Entrar</button>
-                </form>
-            </section>
-            
-            <section className="img-login">
-                <img src={Logo} className='CR' alt="Ilustração do logo Coluna Reta" />
-                <span>Backoffice</span>
-                <img src={LoginImg} className='LoginImg' alt="Ilustração de um usuario fazendo login na aplicação" />
-            </section>
+              <button type="submit">Entrar</button>
+            </form>
+          </div>
         </section>
-    );
-  };
+
+        <section className="right-side">
+          <div className="logo">
+            <img
+              src={Logo}
+              className="CR"
+              alt="Ilustração do logo Coluna Reta"
+            />
+            <span> Backoffice</span>
+          </div>
+          <img
+            src={LoginImg}
+            className="LoginImg"
+            alt="Ilustração de um usuario fazendo login na aplicação"
+          />
+        </section>
+      </section>
+    </section>
+  );
+};
