584010b2957890ca6c0054a50a9cace7bd76dcdb
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-08-10 22:44:19 -0300

    feat: Backoficce page

--- app/src/pages/Backoficce/index.tsx
+++ app/src/pages/Backoficce/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import './style.css'
 import Header from '../../components/Header'
+import Navbar from '../../components/Navbar'
+import Home from '../../components/Home'
 
 const Backoficce = () => {
   return (
       <main className='backoficce_main-container'>
-         <Header></Header>
+         <Header/>
+         <section className='backoficce_content-container'>
+           <Navbar/>
+           <section className='backoficce-content'>
+             <Home/>
+           </section>
+         </section>
       </main>
     )
 }
