985473622ce99d0ebeca42eeac00516c7d62f2fd
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-08-12 16:27:29 -0300

    feat: Gear icon

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router";
 import "./App.css";
+import Backoficce from "./pages/Backoficce";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Router />
+    // </BrowserRouter>
+    <Backoficce></Backoficce>
   );
 }
 
