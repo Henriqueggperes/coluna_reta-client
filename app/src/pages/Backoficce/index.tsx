67830239ee8d1093cfa0b80760699734ae21ea69
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-08-12 15:52:17 -0300

    feat: Navbar options logic to backoficce

--- app/src/pages/Backoficce/index.tsx
+++ app/src/pages/Backoficce/index.tsx
@@ -2,18 +2,26 @@ import './style.css'
 import Header from '../../components/Header'
 import Navbar from '../../components/Navbar'
 import Home from '../../components/Home'
-import Alunos from '../../components/StudentsList'
-
+import React from 'react'
+import Lists from '../../components/Lists'
+import { useState } from 'react'
+import { render } from 'react-dom'
 
 
 const Backoficce = () => {
+  const [currentOption,setCurrentOption] = useState<string>("")
+  const getCurrentOption = (option:string)=>{
+    setCurrentOption(option);
+  }
   return (
       <main className='backoficce_main-container'>
          <Header/>
          <section className='backoficce_content-container'>
-           <Navbar/>
+           <Navbar 
+           navOptionSelected = {getCurrentOption}
+           />
            <section className='backoficce-content'>
-               <Alunos></Alunos>
+             {currentOption? <Lists/>: <Home/>}
            </section>
          </section>
       </main>
