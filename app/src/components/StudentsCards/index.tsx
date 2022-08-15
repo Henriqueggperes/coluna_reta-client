b9383e163833fc00c742482b5ea6d5d499bee7e3
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-08-15 19:28:56 -0300

    feat: searchbar  logic

--- app/src/components/StudentsCards/index.tsx
+++ app/src/components/StudentsCards/index.tsx
@@ -3,23 +3,39 @@ import { Students } from "../../mocks/Students/students.mocks";
 import students_icon from "./../../assets/icons/students_icon.svg"
 import "./style.css";
 
-const StudentsCards = () => {
+//A PROP searchedStudents É DO TIPO: studentObj[]
+const StudentsCards = (props:{searchStudents:any}) => {
+
+  console.log(props.searchStudents)
   return (
-    <section className="students_list_cards-container">
-      {Students.map((item) => (
-        <div className="StudentsCard">
+    <>
+      {props.searchStudents.length!=0? props.searchStudents.map((item:any) => (
+        <div className="StudentsCard" key={item.id}>
           <img src={students_icon} alt="" />
           <div className="CardInfo">
-            <p className="student_card_info-name student-info">
-              <b>Nome:</b> {item.name}
-            </p>
-            <p className="student_card_info-institution student-info">
-              <b>Instituição:</b> {item.institution}
-            </p> 
+            <div className="student_card_info-name student-info">
+              <label>Nome:</label> <span>{item.name}</span>
+            </div>
+            <div className="student_card_info-institution student-info">
+              <label>Instituição:</label> <span> </span>{item.institution}
+            </div> 
+          </div>
+        </div>
+      )):
+      Students.map((item) => (
+        <div className="StudentsCard" key={item.id}>
+          <img src={students_icon} alt="" />
+          <div className="CardInfo">
+            <div className="student_card_info-name student-info">
+              <label>Nome:</label> <span>{item.name}</span>
+            </div>
+            <div className="student_card_info-institution student-info">
+              <label>Instituição:</label> <span> </span>{item.institution}
+            </div> 
           </div>
         </div>
       ))}
-    </section>
+    </>
   );
 };
 
