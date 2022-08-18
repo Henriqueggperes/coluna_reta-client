652ebcf4cee13a497c559cd54766add0a272e9da
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-08-18 20:07:49 -0300

    feat: project types

--- app/src/types/types.ts
+++ app/src/types/types.ts
@@ -1,5 +1,16 @@
 export interface LoginInterface {
   email: string;
-  password: string;
+  passwordHash: string;
 }
 
+export interface sValueObj {
+  search: string;
+}
+
+export interface studentObj {
+  id: number; //**  MUDAR PARA STRING DEPOIS ( VAI RECEBER UM ID HASH ) **
+  name: string;
+  birthDate: string;
+  number: number;
+  institution: string;
+}
